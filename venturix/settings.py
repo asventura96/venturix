00c86ac87d1e7248d86ef5b1ec8aa90cd6a2ec8f
André Ventura <ti@asventura.com.br>
2024-12-19 19:38:52 -0300

    Ajustes no arquivo settings.py

--- venturix/settings.py
+++ venturix/settings.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'siga.middleware.LoginRequiredMiddleware',
+    'venturix.middleware.LoginRequiredMiddleware',
 ]
 
 # Configuração dos templates (TEMPLATES)
